b88b7d6c9f252a2713c91abb87c338767d2306a1
JIZHENQIANG <49663620+JIZHENQIANG@users.noreply.github.com>
2019-04-16 09:20:58 +0800

    Add files via upload

---
+++ shiyan.java
@@ -0,0 +1,15 @@
+﻿    public static void main(String[] args) {
+        double a;
+        a=Math.asin(0.78);
+        System.out.print("反正弦值：");
+        System.out.println(a);
+        a=Math.cos(6);
+        System.out.print("余弦值：");
+        System.out.println(a);
+        a=Math.log(2.71828);
+        System.out.print("自然对数：");
+        System.out.println(a);
+        a=Math.sqrt(16);
+        System.out.print("开平方：");
+        System.out.println(a);
+    }
